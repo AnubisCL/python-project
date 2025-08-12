16d71d51677b738851734d2a09d8960083015c2d
chailei <anubiscl@163.com>
2025-08-12 23:52:14 +0800

    feat(shasha): 更新视频下载功能
    - 新增 N_m3u8DL-RE 下载工具支持
    - 修复部分视频无法下载的问题
    - 优化多线程下载逻辑
    - 更新视频 ID 和线路配置

--- drission-page/shasha.py
+++ drission-page/shasha.py
@@ -4,8 +4,8 @@ import os
 from DrissionPage._configs.chromium_options import ChromiumOptions
 from DrissionPage._pages.web_page import WebPage
 
-VIDEO_ID = '498655'  # todo 1.视频id
-video_line = ''  # todo 2.视频线路,可以为空，需要‘/56’
+VIDEO_ID = '530497'  # todo 1.视频id
+video_line = '/61'  # todo 2.视频线路,可以为空，需要‘/56’
 
 url_index = 'https://shasha.one/v/' + VIDEO_ID
 video_index = 'https://shasha.one/' + VIDEO_ID + '/'
@@ -25,17 +25,18 @@ def get_video_m3u8():
     page.set.load_mode.none()
     page.get(url_index)
     items = page.eles('xpath://a[@class="seq border "]')
-    # episode_length = items.__len__()
+    episode_length = items.__len__()
     # episode_length = 1
-    episode_length = [2,3,4,5,6,7,8,9,11]    # todo 1.自定义集数，打开注释2，3
+    # episode_length = [2,3,4,5,6,7,8,9,11]    # todo 1.自定义集数，打开注释2，3
 
     def fetch_m3u8(id):
 
-        url = video_index + str(id) + video_line # todo 3.自定义集数
-        # url = video_index + str(id + 1) + video_line
+        # url = video_index + str(id) + video_line # todo 3.自定义集数
+        url = video_index + str(id + 1) + video_line
 
         tab = page.new_tab(url)
         tab.listen.start(targets='.m3u8', method='GET')  # 开启监听验证码请求抓包
+        # tab.listen.start(targets='cdn.wlcdn88.com', method='GET')  # 开启监听验证码请求抓包
         m3u8 = tab.listen.wait()  # 抓取m3u8请求
         m3u8_url = m3u8.url
         tab.stop_loading()
@@ -44,8 +45,8 @@ def get_video_m3u8():
 
     # 设置线程池参数，最大线程数为10
     with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
-        future_to_id = {executor.submit(fetch_m3u8, id): id for id in episode_length} # todo 2.自定义集数
-        # future_to_id = {executor.submit(fetch_m3u8, id): id for id in range(episode_length)}
+        # future_to_id = {executor.submit(fetch_m3u8, id): id for id in episode_length} # todo 2.自定义集数
+        future_to_id = {executor.submit(fetch_m3u8, id): id for id in range(episode_length)}
         for future in concurrent.futures.as_completed(future_to_id):
             id = future_to_id[future]
             try:
@@ -61,7 +62,7 @@ def get_video_m3u8():
             f.write(f'{episode_id}: {m3u8_url}\n')
 
 
-def download_video():
+def download_video(use_N_m3u8DL_RE=True):
     import subprocess
     import concurrent.futures
 
@@ -74,12 +75,22 @@ def download_video():
         # command = f"ffmpeg -i {m3u8_url} -map 0 -c copy {output_file}"
         subprocess.run(command, shell=True)
 
+    def download_N_m3u8DL_RE(episode_id, m3u8_url):
+        savepath = f"{video_path}"
+        filename = f"{episode_id}_{VIDEO_ID}.mp4"
+        command = f"/Users/anubis/PycharmProjects/python-project/drission-page/shasha/N_m3u8DL-RE \"{m3u8_url}\" --save-dir {savepath} --save-name {filename}"
+        subprocess.run(command, shell=True)
+
     with open(path, 'r') as f:
         episode_map = {int(line.split(': ')[0]): line.split(': ')[1].strip() for line in f}
 
     with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
-        future_to_id = {executor.submit(download_m3u8, episode_id, m3u8_url): episode_id for episode_id, m3u8_url in
-                        episode_map.items()}
+        if use_N_m3u8DL_RE:
+            future_to_id = {executor.submit(download_N_m3u8DL_RE, episode_id, m3u8_url): episode_id for episode_id, m3u8_url in
+                            episode_map.items()}
+        else:
+            future_to_id = {executor.submit(download_m3u8, episode_id, m3u8_url): episode_id for episode_id, m3u8_url in
+                            episode_map.items()}
         for future in concurrent.futures.as_completed(future_to_id):
             id = future_to_id[future]
             try:
@@ -112,5 +123,5 @@ def merge_video():
 
 if __name__ == '__main__':
     # get_video_m3u8()
-    download_video()
+    download_video(use_N_m3u8DL_RE=True)
     # merge_video()
